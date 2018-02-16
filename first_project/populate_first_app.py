cdf555e21cccd5d3a8862dfe28280f9a538d1305
Simon <simonmarkholland@gmail.com>
2018-02-16 15:06:34 +0700

    Created script to populate database
    Works in Python 3

--- first_project/populate_first_app.py
+++ first_project/populate_first_app.py
@@ -1,19 +1,32 @@
+# These 2 lines allows the code to work in Python 3
+import sys
+sys.path.append('..')
+
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','first_project.settings')
 
 import django
 django.setup()
 
-import randomfrom first_app.models import AccessRecord, Webpage, Topic
-from faker import faker
+import random
+
+# Python3 alternative should os.environ call wasn't needed ...
+# from . import first_app
+# from .first_app import AccessRecord, Webpage, Topic
+
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
 
 fakegen = Faker()
 topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
 
 def add_topic():
     t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
     t.save()
     return t
+
 
 def populate(N=5):
 
@@ -22,8 +35,12 @@ def populate(N=5):
 
         fake_url = fakegen.url()
         fake_date = fakegen.date()
-        fake_name = fakename.company()
+        fake_name = fakegen.company()
+        webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
 
-        webpg = =Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
 
-        acc_rec + AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+if __name__ == "__main__":
+    print('Populating script!')
+    populate(20)
+    print('Populating complete!')
